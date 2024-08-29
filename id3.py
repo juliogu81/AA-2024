b743103078e63229225966fcf2e8fd3ddfd7717a
leandro-pereira01 <leo.pereira1932@gmail.com>
2024-08-29 17:19:58 -0300

    inicio de informe e fixes al id3

--- id3.py
+++ id3.py
@@ -1,58 +1,88 @@
 import numpy as np
-from math import log2
+from sklearn import preprocessing, model_selection
 import pandas as pd
-from itertools import combinations
 
 def calcular_entropia(etiqueta):
-    # Contar la frecuencia de cada etiqueta
-    frecuencia = {}
-    for item in etiqueta:
-        if item in frecuencia:
-            frecuencia[item] += 1
-        else:
-            frecuencia[item] = 1
-    
-    # Calcular la probabilidad de cada etiqueta
-    total = len(etiqueta)
-    proporciones = [frecuencia[item] / total for item in frecuencia]
-    
-    # Calcular la entropía
-    return -sum(p * log2(p) for p in proporciones)
+    valores, conteos = np.unique(etiqueta, return_counts=True)
+    probabilidades = conteos / len(etiqueta)
+    entropia = -np.sum(probabilidades * np.log2(probabilidades))
+    return entropia
 
-def calcular_ganancia_discretizado(intervalos, etiqueta):
-    # Se considera que 'atributo_a_calcular' es 'None' para valores discretos
-    return calcular_ganancia(intervalos, etiqueta, atributo_a_calcular=None)
-
-def calcular_ganancia(atributos, etiqueta, atributo_a_calcular):
-    entropia_original = calcular_entropia(etiqueta)
+def calcular_ganancia(atributos, etiqueta, atributo_a_calcular, puntos_corte):
+    entropia_inicial = calcular_entropia(etiqueta)
     
-    if atributo_a_calcular is None:
-        # En el caso de atributos discretos, usamos los intervalos calculados
-        valores, conteos = np.unique(atributos, return_counts=True)
-        entropia_atributo = sum((conteos[i] / sum(conteos)) * calcular_entropia(etiqueta[atributos == v]) for i, v in enumerate(valores))
-    else:
-        # En el caso de atributos continuos, usamos la función calculada anteriormente
+    if puntos_corte is None or len(puntos_corte) == 0:
         valores, conteos = np.unique(atributos[:, atributo_a_calcular], return_counts=True)
-        entropia_atributo = sum((conteos[i] / sum(conteos)) * calcular_entropia(etiqueta[atributos[:, atributo_a_calcular] == v]) for i, v in enumerate(valores))
+        entropia_condicional = sum((conteos[i] / sum(conteos)) * calcular_entropia(etiqueta[atributos[:, atributo_a_calcular] == v]) for i, v in enumerate(valores))
+    
+    # Si hay un solo punto de corte
+    elif len(puntos_corte) == 1:
+        punto_corte = puntos_corte[0]
+        menores = atributos[:, atributo_a_calcular].astype(float) <= punto_corte
+        mayores = atributos[:, atributo_a_calcular].astype(float) > punto_corte
+        
+        if not np.any(menores) or not np.any(mayores):
+            return 0  # No hay ganancia de información si un subconjunto está vacío
+        
+        entropia_menores = calcular_entropia(etiqueta[menores])
+        entropia_mayores = calcular_entropia(etiqueta[mayores])
+        
+        peso_menores = np.sum(menores) / len(atributos)
+        peso_mayores = np.sum(mayores) / len(atributos)
+        
+        entropia_condicional = (peso_menores * entropia_menores + peso_mayores * entropia_mayores)
+    
+    # Si hay dos puntos de corte
+    elif len(puntos_corte) == 2:
+        punto_corte_1 = puntos_corte[0]
+        punto_corte_2 = puntos_corte[1]
+        rango_1 = atributos[:, atributo_a_calcular].astype(float) <= punto_corte_1
+        rango_2 = (atributos[:, atributo_a_calcular].astype(float) > punto_corte_1) & (atributos[:, atributo_a_calcular].astype(float) <= punto_corte_2)
+        rango_3 = atributos[:, atributo_a_calcular].astype(float) > punto_corte_2
+        
+        if not np.any(rango_1) or not np.any(rango_2) or not np.any(rango_3):
+            return 0  # No hay ganancia de información si un subconjunto está vacío
+        
+        entropia_rango_1 = calcular_entropia(etiqueta[rango_1])
+        entropia_rango_2 = calcular_entropia(etiqueta[rango_2])
+        entropia_rango_3 = calcular_entropia(etiqueta[rango_3])
+        
+        peso_rango_1 = np.sum(rango_1) / len(atributos)
+        peso_rango_2 = np.sum(rango_2) / len(atributos)
+        peso_rango_3 = np.sum(rango_3) / len(atributos)
+        
+        entropia_condicional = (peso_rango_1 * entropia_rango_1 +
+                                peso_rango_2 * entropia_rango_2 +
+                                peso_rango_3 * entropia_rango_3)
+    
+    else:
+        return 0  # Caso inválido, solo permitimos 1 o 2 puntos de corte
+    
+    ganancia = entropia_inicial - entropia_condicional
+    return ganancia
 
-    return entropia_original - entropia_atributo
+def _es_binario(atributo):
+    return len(np.unique(atributo)) <= 2
 
-
-def _etiqueta_mas_comun(self, etiqueta):
-        # Encuentra la etiqueta que aparece más frecuentemente
-        (valores, conteos) = np.unique(etiqueta, return_counts=True)
-        indice = np.argmax(conteos)
-        return valores[indice]
-
-
-def train_test_split(dataset, test_size=0.2, random_state=None):
-    np.random.seed(random_state)
-    indices = np.random.permutation(len(dataset))
-    test_indices = indices[:int(test_size * len(dataset))]
-    train_indices = indices[int(test_size * len(dataset)):]
-    train = dataset.iloc[train_indices]
-    test = dataset.iloc[test_indices]
-    return train, test
+def _encontrar_mejores_puntos_corte(atributo, etiqueta, max_range_split):
+    puntos_corte = []
+    datos_ordenados = sorted(zip(atributo, etiqueta), key=lambda x: x[0])
+    
+    for i in range(1, len(datos_ordenados)):
+        valor_actual, etiqueta_actual = datos_ordenados[i]
+        valor_anterior, etiqueta_anterior = datos_ordenados[i - 1]
+        
+        if etiqueta_actual != etiqueta_anterior:
+            try:
+                punto_corte = (float(valor_actual) + float(valor_anterior)) / 2
+                puntos_corte.append(punto_corte)
+            except ValueError:
+                continue
+    
+    if len(puntos_corte) > max_range_split:
+        puntos_corte = puntos_corte[:max_range_split]
+    
+    return puntos_corte
 
 class ArbolDecision:
     def __init__(self):
@@ -62,155 +92,121 @@ class ArbolDecision:
         self.arbol = self._construir_arbol(atributos, etiqueta, max_range_split)
     
     def _construir_arbol(self, atributos, etiqueta, max_range_split):
-        # Si todas las etiquetas son iguales, devuelve la etiqueta
         if len(np.unique(etiqueta)) == 1:
-            return etiqueta[0]
+            return np.unique(etiqueta)[0]
         
-        # Si no quedan más atributos, devolver la etiqueta más común
-        if atributos.shape[1] == 0:
-            return self._etiqueta_mas_comun(etiqueta)
-
-        es_continuo = False
-        # Encuentra la característica con la mayor ganancia de información. Si es continua se debe dividir en max_range_split
-        ganancia_max = - float('inf')
+        if atributos.shape[1] == 0 or len(atributos) == 0:
+            return np.bincount(etiqueta).argmax()
+        
         mejor_atributo = None
-        print(atributos.shape[1])
-        for atri in range(atributos.shape[1]):
-            if self._es_binario(atributos[:, atri]):
-                ganancia = calcular_ganancia(atributos, etiqueta, atri)
-                if ganancia > ganancia_max:
-                    ganancia_max = ganancia
-                    mejor_atributo = atri
-                    es_continuo = False
-            else:
-                puntos_corte = self._encontrar_mejores_puntos_corte(atributos[:, atri], etiqueta, max_range_split)
-                intervalos = np.digitize(atributos[:, atri], puntos_corte)
-                ganancia = calcular_ganancia_discretizado(intervalos, etiqueta)
-                
-                if ganancia > ganancia_max:
-                    ganancia_max = ganancia
-                    mejor_atributo = atri
-                    es_continuo = True
-                    puntos_corte_optimos = puntos_corte
-
-        # Crea los nodos del árbol
-        arbol = {mejor_atributo: {}}
-        if (not es_continuo):
-            values = np.unique(atributos[:, mejor_atributo])
-            for v in values:
-                subset_X = atributos[atributos[:, mejor_atributo] == v]
-                subset_y = etiqueta[atributos[:, mejor_atributo] == v]
-                
-                # Eliminar el mejor atributo del subconjunto antes de la recursión
-                subset_X = np.delete(subset_X, mejor_atributo, axis=1)
-                
-                subtree = self._construir_arbol(subset_X, subset_y, max_range_split)
-                arbol[mejor_atributo][v] = subtree
+        mejor_ganancia = -1
+        mejor_puntos_corte = None
         
-        else:
-            intervalos = np.digitize(atributos[:, mejor_atributo], puntos_corte_optimos)
-            for i in range(len(puntos_corte_optimos) + 1):
-                subset_X = atributos[intervalos == i]
-                subset_y = etiqueta[intervalos == i]
-                subset_X = np.delete(subset_X, mejor_atributo, axis=1)
-                subtree = self._construir_arbol(subset_X, subset_y, max_range_split)
-                arbol[mejor_atributo][i] = subtree
+        for i in range(atributos.shape[1]):
+            if _es_binario(atributos[:, i]):
+                ganancia = calcular_ganancia(atributos, etiqueta, i, None)
+                puntos_corte = None
+            else:
+                puntos_corte = _encontrar_mejores_puntos_corte(atributos[:, i], etiqueta, max_range_split)
+                ganancia = calcular_ganancia(atributos, etiqueta, i, puntos_corte)
             
+            if ganancia > mejor_ganancia:
+                mejor_ganancia = ganancia
+                mejor_atributo = i
+                mejor_puntos_corte = puntos_corte
+        
+        if mejor_ganancia == 0 or mejor_atributo is None:
+            return np.bincount(etiqueta).argmax()
+        
+        arbol = {}
+        arbol['atributo'] = mejor_atributo
+        arbol['puntos_corte'] = mejor_puntos_corte
+        
+        if mejor_puntos_corte is None:
+            valores = np.unique(atributos[:, mejor_atributo])
+            for valor in valores:
+                indices = atributos[:, mejor_atributo] == valor
+                if len(indices) > 0:
+                    arbol[valor] = self._construir_arbol(atributos[indices], etiqueta[indices], max_range_split)
+        else:
+            if len(mejor_puntos_corte) == 1:
+                punto_corte = mejor_puntos_corte[0]
+                menores = atributos[:, mejor_atributo].astype(float) <= punto_corte
+                mayores = atributos[:, mejor_atributo].astype(float) > punto_corte
+                if np.any(menores):
+                    arbol[f'<= {punto_corte}'] = self._construir_arbol(atributos[menores], etiqueta[menores], max_range_split)
+                if np.any(mayores):
+                    arbol[f'> {punto_corte}'] = self._construir_arbol(atributos[mayores], etiqueta[mayores], max_range_split)
+            elif len(mejor_puntos_corte) == 2:
+                punto_corte_1 = mejor_puntos_corte[0]
+                punto_corte_2 = mejor_puntos_corte[1]
+                rango_1 = atributos[:, mejor_atributo].astype(float) <= punto_corte_1
+                rango_2 = (atributos[:, mejor_atributo].astype(float) > punto_corte_1) & (atributos[:, mejor_atributo].astype(float) <= punto_corte_2)
+                rango_3 = atributos[:, mejor_atributo].astype(float) > punto_corte_2
+                if np.any(rango_1):
+                    arbol[f'<= {punto_corte_1}'] = self._construir_arbol(atributos[rango_1], etiqueta[rango_1], max_range_split)
+                if np.any(rango_2):
+                    arbol[f'> {punto_corte_1} y <= {punto_corte_2}'] = self._construir_arbol(atributos[rango_2], etiqueta[rango_2], max_range_split)
+                if np.any(rango_3):
+                    arbol[f'> {punto_corte_2}'] = self._construir_arbol(atributos[rango_3], etiqueta[rango_3], max_range_split)
         
         return arbol
-
-    def _es_binario(self, atributo):
-        # Verifica si el atributo es binario
-        return len(np.unique(atributo)) <= 2
     
-    def _encontrar_puntos_corte(self, atributo, etiqueta):
-        # Encuentra puntos de corte óptimos
-        puntos_corte = []
-        # Ordena los atributos y etiquetas juntos
-        datos_ordenados = sorted(zip(atributo, etiqueta), key=lambda x: x[0])
-        for i in range(1, len(datos_ordenados)):
-            if datos_ordenados[i-1][1] != datos_ordenados[i][1]:
-                punto_corte = (datos_ordenados[i-1][0] + datos_ordenados[i][0]) / 2
-                puntos_corte.append(punto_corte)
-        return puntos_corte
-
-
-    def _encontrar_mejores_puntos_corte(self, atributo, etiqueta, max_range_split):
-        puntos_corte = self._encontrar_puntos_corte(atributo, etiqueta)
-        mejor_ganancia = -float('inf')
-        mejores_puntos = None
-        # Probar combinaciones de puntos de corte
-        for num_puntos in range(1, max_range_split):
-            for combinacion in combinations(puntos_corte, num_puntos):
-                combinacion = sorted(combinacion)
-                intervalos = np.digitize(atributo, combinacion)
-                ganancia = calcular_ganancia_discretizado(intervalos, etiqueta)
+    def predict(self, x):
+        nodo = self.arbol
+        while isinstance(nodo, dict):
+            atributo = nodo['atributo']
+            puntos_corte = nodo['puntos_corte']
+            
+            if puntos_corte is None:
+                valor = x[atributo]
+                if valor in nodo:
+                    nodo = nodo[valor]
+                else:
+                    return None  # Valor no esperado
+            else:
+                if len(puntos_corte) == 1:
+                    punto_corte = puntos_corte[0]
+                    if x[atributo] <= punto_corte:
+                        nodo = nodo.get(f'<= {punto_corte}', None)
+                    else:
+                        nodo = nodo.get(f'> {punto_corte}', None)
+                elif len(puntos_corte) == 2:
+                    punto_corte_1 = puntos_corte[0]
+                    punto_corte_2 = puntos_corte[1]
+                    if x[atributo] <= punto_corte_1:
+                        nodo = nodo.get(f'<= {punto_corte_1}', None)
+                    elif x[atributo] <= punto_corte_2:
+                        nodo = nodo.get(f'> {punto_corte_1} y <= {punto_corte_2}', None)
+                    else:
+                        nodo = nodo.get(f'> {punto_corte_2}', None)
                 
-                if ganancia > mejor_ganancia:
-                    mejor_ganancia = ganancia
-                    mejores_puntos = combinacion
+        return nodo
 
-        return mejores_puntos    
-
-    def predict(self, X):
-        # Maneja tanto una lista de ejemplos como un solo ejemplo
-        if X.ndim == 1:
-            return self._predecir_ejemplo(X, self.arbol)
-        else:
-            return [self._predecir_ejemplo(ejemplo, self.arbol) for ejemplo in X]
-
-    def _predecir_ejemplo(self, ejemplo, arbol):
-        if not isinstance(arbol, dict):
-            return arbol
-
-        atributo = list(arbol.keys())[0]
-        valor = ejemplo[atributo]
-
-        if valor in arbol[atributo]:
-            subarbol = arbol[atributo][valor]
-            return self._predecir_ejemplo(ejemplo, subarbol)
-        else:
-            return None
-
-
-
-
-
-
-tree = ArbolDecision()
-
+# Leer el archivo y dividir en conjunto de entrenamiento y prueba
 DATASET_FILE = 'lab1_dataset.csv'
+dataset = pd.read_csv(DATASET_FILE, sep=",", header=None).add_prefix("c")
+dataset = dataset.drop(dataset.columns[0], axis=1)
 
-dataset = pd.read_csv(DATASET_FILE, sep=",", header=0)
-print(
-    f"{dataset.shape[0]} records read from {DATASET_FILE}\n{dataset.shape[1]} attributes found"
-)
-dataset.head(10)
+# Convertir todos los atributos a valores numéricos
+le = preprocessing.LabelEncoder()
+for column in dataset.columns:
+    dataset[column] = le.fit_transform(dataset[column])
 
+train, test = model_selection.train_test_split(dataset, test_size=0.2, random_state=42)
 
-
-
-train, test = train_test_split(dataset, test_size=0.85, random_state=42)
-print(f"{train.shape[0]} samples for training, {test.shape[0]} samples for testing")
-train.head(10)
-
-#descartamos el primer atributo
-train.drop(columns=train.columns[0], inplace=True)
-test.drop(columns=test.columns[0], inplace=True)
-
-
-#el atributo objetivo es el primero
+# Entrenamiento del modelo
 atributos = train.iloc[:, 1:].values
 etiqueta = train.iloc[:, 0].values
 
+arbol = ArbolDecision()
+arbol.fit(atributos, etiqueta, 3)
 
-
-tree.fit(atributos, etiqueta, 2)
-
-#validar el arbol con test
+# Validar el árbol con el conjunto de prueba
 atributos_test = test.iloc[:, 1:].values
 etiqueta_test = test.iloc[:, 0].values
 
-predicciones = [tree.predict(x) for x in atributos_test]
-precision = sum(predicciones == etiqueta_test) / len(etiqueta_test)
-print(precision)
+# Hacer predicciones y calcular la precisión
+predicciones = [arbol.predict(x) for x in atributos_test]
+precision = np.sum(np.array(predicciones) == etiqueta_test) / len(etiqueta_test)
+print(f"Precisión: {precision}")
