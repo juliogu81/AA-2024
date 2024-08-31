18b60ac0753df395c6a30895018b25f705e456de
juliogu <146482825+juliogu@users.noreply.github.com>
2024-08-30 21:03:27 -0300

    cambios en preprocess

--- id3_preprocess.py
+++ id3_preprocess.py
@@ -9,7 +9,6 @@ import pandas as pd
 from itertools import combinations
 from sklearn.model_selection import KFold
 
-
 def calcular_entropia(etiqueta):
     valores, conteos = np.unique(etiqueta, return_counts=True)
     probabilidades = conteos / len(etiqueta)
@@ -20,14 +19,14 @@ def calcular_ganancia(atributos, etiqueta, atributo_a_calcular, puntos_corte):
     entropia_inicial = calcular_entropia(etiqueta)
     
     if puntos_corte is None or len(puntos_corte) == 0:
-        valores, conteos = np.unique(atributos[:, atributo_a_calcular], return_counts=True)
-        entropia_condicional = sum((conteos[i] / sum(conteos)) * calcular_entropia(etiqueta[atributos[:, atributo_a_calcular] == v]) for i, v in enumerate(valores))
+        valores, conteos = np.unique(atributos.iloc[:, atributo_a_calcular], return_counts=True)
+        entropia_condicional = sum((conteos[i] / sum(conteos)) * calcular_entropia(etiqueta[atributos.iloc[:, atributo_a_calcular] == v]) for i, v in enumerate(valores))
     
     # Si hay un solo punto de corte
     elif len(puntos_corte) == 1:
         punto_corte = puntos_corte[0]
-        menores = atributos[:, atributo_a_calcular].astype(float) <= punto_corte
-        mayores = atributos[:, atributo_a_calcular].astype(float) > punto_corte
+        menores = atributos.iloc[:, atributo_a_calcular].astype(float) <= punto_corte
+        mayores = atributos.iloc[:, atributo_a_calcular].astype(float) > punto_corte
         
         if not np.any(menores) or not np.any(mayores):
             return 0  # No hay ganancia de información si un subconjunto está vacío
@@ -44,9 +43,9 @@ def calcular_ganancia(atributos, etiqueta, atributo_a_calcular, puntos_corte):
     elif len(puntos_corte) == 2:
         punto_corte_1 = puntos_corte[0]
         punto_corte_2 = puntos_corte[1]
-        rango_1 = atributos[:, atributo_a_calcular].astype(float) <= punto_corte_1
-        rango_2 = (atributos[:, atributo_a_calcular].astype(float) > punto_corte_1) & (atributos[:, atributo_a_calcular].astype(float) <= punto_corte_2)
-        rango_3 = atributos[:, atributo_a_calcular].astype(float) > punto_corte_2
+        rango_1 = atributos.iloc[:, atributo_a_calcular].astype(float) <= punto_corte_1
+        rango_2 = (atributos.iloc[:, atributo_a_calcular].astype(float) > punto_corte_1) & (atributos.iloc[:, atributo_a_calcular].astype(float) <= punto_corte_2)
+        rango_3 = atributos.iloc[:, atributo_a_calcular].astype(float) > punto_corte_2
         
         if not np.any(rango_1) or not np.any(rango_2) or not np.any(rango_3):
             return 0  # No hay ganancia de información si un subconjunto está vacío
@@ -90,7 +89,7 @@ def _encontrar_mejores_puntos_corte(atributos, atributo, indice, etiqueta, max_r
     if len(puntos_corte) > max_range_split:
         mejor_ganancia = -float('inf')
         # Probar combinaciones de puntos de corte
-        for num_puntos in range(1, max_range_split):
+        for num_puntos in range(1, max_range_split + 1):
             for combinacion in combinations(puntos_corte, num_puntos):
                 combinacion = sorted(combinacion)
                 ganancia = calcular_ganancia(atributos, etiqueta, indice, combinacion)
@@ -100,14 +99,6 @@ def _encontrar_mejores_puntos_corte(atributos, atributo, indice, etiqueta, max_r
                     puntos_corte = combinacion   
     
     return puntos_corte
-   
-
-
-
-
-
-
-
 
 class ArbolDecision:
     def __init__(self):
@@ -132,7 +123,7 @@ class ArbolDecision:
                 ganancia = calcular_ganancia(atributos, etiqueta, i, None)
                 puntos_corte = None
             else:
-                puntos_corte = _encontrar_mejores_puntos_corte(atributos, atributos[:, i], i, etiqueta, max_range_split)
+                puntos_corte = _encontrar_mejores_puntos_corte(atributos, atributos.iloc[:, i], i, etiqueta, max_range_split)
                 ganancia = calcular_ganancia(atributos, etiqueta, i, puntos_corte)
             
             if ganancia > mejor_ganancia:
@@ -148,16 +139,16 @@ class ArbolDecision:
         arbol['puntos_corte'] = mejor_puntos_corte
         
         if mejor_puntos_corte is None:
-            valores = np.unique(atributos[:, mejor_atributo])
+            valores = np.unique(atributos.iloc[:, mejor_atributo])
             for valor in valores:
-                indices = atributos[:, mejor_atributo] == valor
-                if len(indices) > 0:
+                indices = atributos.iloc[:, mejor_atributo] == valor
+                if np.any(indices):
                     arbol[valor] = self._construir_arbol(atributos[indices], etiqueta[indices], max_range_split, categoricos)
         else:
             if len(mejor_puntos_corte) == 1:
                 punto_corte = mejor_puntos_corte[0]
-                menores = atributos[:, mejor_atributo].astype(float) <= punto_corte
-                mayores = atributos[:, mejor_atributo].astype(float) > punto_corte
+                menores = atributos.iloc[:, mejor_atributo].astype(float) <= punto_corte
+                mayores = atributos.iloc[:, mejor_atributo].astype(float) > punto_corte
                 if np.any(menores):
                     arbol[f'<= {punto_corte}'] = self._construir_arbol(atributos[menores], etiqueta[menores], max_range_split, categoricos)
                 if np.any(mayores):
@@ -165,9 +156,9 @@ class ArbolDecision:
             elif len(mejor_puntos_corte) == 2:
                 punto_corte_1 = mejor_puntos_corte[0]
                 punto_corte_2 = mejor_puntos_corte[1]
-                rango_1 = atributos[:, mejor_atributo].astype(float) <= punto_corte_1
-                rango_2 = (atributos[:, mejor_atributo].astype(float) > punto_corte_1) & (atributos[:, mejor_atributo].astype(float) <= punto_corte_2)
-                rango_3 = atributos[:, mejor_atributo].astype(float) > punto_corte_2
+                rango_1 = atributos.iloc[:, mejor_atributo].astype(float) <= punto_corte_1
+                rango_2 = (atributos.iloc[:, mejor_atributo].astype(float) > punto_corte_1) & (atributos.iloc[:, mejor_atributo].astype(float) <= punto_corte_2)
+                rango_3 = atributos.iloc[:, mejor_atributo].astype(float) > punto_corte_2
                 if np.any(rango_1):
                     arbol[f'<= {punto_corte_1}'] = self._construir_arbol(atributos[rango_1], etiqueta[rango_1], max_range_split, categoricos)
                 if np.any(rango_2):
@@ -208,89 +199,73 @@ class ArbolDecision:
                 
         return nodo
 
-
 if __name__ == '__main__':
-
     # Leer el archivo
     DATASET_FILE = 'lab1_dataset.csv'
     dataset = pd.read_csv(DATASET_FILE, sep=",").add_prefix("c")
 
-    #Se elimina del dataset la primera columna ya que no es un atributo. Corresponde al ID del paciente
+    # Se elimina del dataset la primera columna ya que no es un atributo. Corresponde al ID del paciente
     dataset = dataset.drop(dataset.columns[0], axis=1)
 
     categorical_columns = [1, 4, 5, 6, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18]
 
-    atributos = dataset.iloc[:, 1:].values
-    etiqueta = dataset.iloc[:, 0].values
+    atributos = dataset.iloc[:, 1:].copy()
+    etiqueta = dataset.iloc[:, 0].copy()
 
     # Se convierten los valores numericos utilizando discretizacion
-    atributos_max_range_split_2 = atributos
-    atributos_max_range_split_3 = atributos 
+    atributos_max_range_split_2 = atributos.copy()
+    atributos_max_range_split_3 = atributos.copy()
 
     for col in range(atributos.shape[1]):
         if col not in categorical_columns:
-            punto_corte = _encontrar_mejores_puntos_corte(atributos_max_range_split_2, atributos[:, col], col, etiqueta, 2)
-            atributos[:, col] = pd.cut(atributos[:, col], bins=[-np.inf, punto_corte[0], np.inf], labels=[0, 1]).astype(int)
-            puntos_corte = _encontrar_mejores_puntos_corte(atributos_max_range_split_3, atributos_max_range_split_3[:, col], col, etiqueta, 3)
-            if len(puntos_corte) == 1:
-                atributos_max_range_split_3[:, col] = pd.cut(atributos_max_range_split_3[:, col], bins=[-np.inf, puntos_corte[0], np.inf], labels=[0, 1]).astype(int)
-            else:
-                atributos_max_range_split_3[:, col] = pd.cut(atributos_max_range_split_3[:, col], bins=[-np.inf, puntos_corte[0], puntos_corte[1], np.inf], labels=[0, 1, 2]).astype(int)
+            punto_corte_2 = _encontrar_mejores_puntos_corte(atributos_max_range_split_2, atributos.iloc[:, col], col, etiqueta, 2)
+            atributos_max_range_split_2.iloc[:, col] = pd.cut(atributos.iloc[:, col], bins=[-np.inf] + punto_corte_2 + [np.inf], labels=range(len(punto_corte_2)+1)).astype(int)
+            
+            puntos_corte_3 = _encontrar_mejores_puntos_corte(atributos_max_range_split_3, atributos.iloc[:, col], col, etiqueta, 3)
+            atributos_max_range_split_3.iloc[:, col] = pd.cut(atributos.iloc[:, col], bins=[-np.inf] + puntos_corte_3 + [np.inf], labels=range(len(puntos_corte_3)+1)).astype(int)
 
-    categorical_columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ,21, 22]
+    # Unir atributos y etiquetas de nuevo para dividir el dataset
+    dataset_max_2 = pd.concat([etiqueta, atributos_max_range_split_2], axis=1)
+    dataset_max_3 = pd.concat([etiqueta, atributos_max_range_split_3], axis=1)
 
-    # Se crea el arbol de decision
-    arbol_max_range_split_2 = ArbolDecision()
-    arbol_max_range_split_2.fit(atributos_max_range_split_2, etiqueta, 2, categorical_columns)
-    
-    arbol_max_range_split_3 = ArbolDecision()
-    arbol_max_range_split_3.fit(atributos_max_range_split_3, etiqueta, 3, categorical_columns)
-
-    # Se evalua el arbol de decision
-
-    dataset_max_2 = np.column_stack((etiqueta, atributos_max_range_split_2))
-    dataset_max_3 = np.column_stack((etiqueta, atributos_max_range_split_3))
-
-
-    #Dividimos el dataset en entrenamiento y prueba
+    # Dividir el dataset en entrenamiento y prueba
     train_2, test_2 = model_selection.train_test_split(dataset_max_2, test_size=0.2, random_state=42)
-    train_3, test_3 = model_selection.train_test_split(dataset_max_3, test_size=0.2, random_state=42)   
+    train_3, test_3 = model_selection.train_test_split(dataset_max_3, test_size=0.2, random_state=42)
 
+    # Entrenamiento del modelo. Separar etiquetas y atributos
+    atributos_2 = train_2.iloc[:, 1:]
+    etiqueta_2 = train_2.iloc[:, 0]
+    atributos_test_2 = test_2.iloc[:, 1:]
+    etiqueta_test_2 = test_2.iloc[:, 0]
 
-    train_2 = pd.DataFrame(train_2)
-    train_3 = pd.DataFrame(train_3)
-    test_2 = pd.DataFrame(test_2)
-    test_3 = pd.DataFrame(test_3)
+    atributos_3 = train_3.iloc[:, 1:]
+    etiqueta_3 = train_3.iloc[:, 0]
+    atributos_test_3 = test_3.iloc[:, 1:]
+    etiqueta_test_3 = test_3.iloc[:, 0]
 
-    # Entrenamiento del modelo. La primer columna es el cid (funcion objetivo). Se separa en etiqueta, el resto son atributos
-    atributos_2 = train_2.iloc[:, 1:].values
-    etiqueta_2 = train_2.iloc[:, 0].values
+    # Crear y entrenar el árbol de decisión
+    arbol_max_range_split_2 = ArbolDecision()
+    arbol_max_range_split_2.fit(atributos_2, etiqueta_2, 2, categorical_columns)
 
-    atributos_3 = train_3.iloc[:, 1:].values
-    etiqueta_3 = train_3.iloc[:, 0].values
+    arbol_max_range_split_3 = ArbolDecision()
+    arbol_max_range_split_3.fit(atributos_3, etiqueta_3, 3, categorical_columns)
 
+    # Evaluar el modelo
+    predicciones_m2_train = [arbol_max_range_split_2.predict(x) for x in atributos_2.values]
+    predicciones_m2_test = [arbol_max_range_split_2.predict(x) for x in atributos_test_2.values]
+    precision_m2_train = np.sum(np.array(predicciones_m2_train) == etiqueta_2.values) / len(etiqueta_2)
+    precision_m2_test = np.sum(np.array(predicciones_m2_test) == etiqueta_test_2.values) / len(etiqueta_test_2)
 
-    # Conjunto de prueba
-    atributos_test_2 = test_2.iloc[:, 1:].values
-    etiqueta_test_2 = test_2.iloc[:, 0].values
-
-    atributos_test_3 = test_3.iloc[:, 1:].values
-    etiqueta_test_3 = test_3.iloc[:, 0].values
-
-    # Se evalua el arbol de decision
-    predicciones_m2_train = [arbol_max_range_split_2.predict(x) for x in atributos_2]
-    predicciones_m2_test = [arbol_max_range_split_2.predict(x) for x in atributos_test_2]
-    precision_m2_train = np.sum(np.array(predicciones_m2_train) == etiqueta_2) / len(etiqueta_2)
-    precision_m2_test = np.sum(np.array(predicciones_m2_test) == etiqueta_test_2) / len(etiqueta_test_2)
     print("ALgoritmo ID3 con max_iter_split = 2:")
     print(f"Precisión con datos de entrenamiento: {precision_m2_train * 100}%")
     print(f"Precisión con datos de prueba: {precision_m2_test * 100}%")
     print('\n')
 
-    predicciones_m3_train = [arbol_max_range_split_3.predict(x) for x in atributos_3]
-    predicciones_m3_test = [arbol_max_range_split_3.predict(x) for x in atributos_test_3]
-    precision_m3_train = np.sum(np.array(predicciones_m3_train) == etiqueta_3) / len(etiqueta_3)
-    precision_m3_test = np.sum(np.array(predicciones_m3_test) == etiqueta_test_3) / len(etiqueta_test_3)
+    predicciones_m3_train = [arbol_max_range_split_3.predict(x) for x in atributos_3.values]
+    predicciones_m3_test = [arbol_max_range_split_3.predict(x) for x in atributos_test_3.values]
+    precision_m3_train = np.sum(np.array(predicciones_m3_train) == etiqueta_3.values) / len(etiqueta_3)
+    precision_m3_test = np.sum(np.array(predicciones_m3_test) == etiqueta_test_3.values) / len(etiqueta_test_3)
+
     print("ALgoritmo ID3 con max_iter_split = 3:")
     print(f"Precisión con datos de entrenamiento: {precision_m3_train * 100}%")
     print(f"Precisión con datos de prueba: {precision_m3_test * 100}%")
